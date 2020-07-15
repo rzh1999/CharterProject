3bd4df5d568a178b9174b6dc9f924f2f8680507b
Bill <william.hine2019@gmail.com>
2020-07-15 16:29:03 -0400

    Init

---
+++ Charter/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Charter.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
