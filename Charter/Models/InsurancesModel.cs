c28d2757e97e56dbcc30d4b53184cabd22b610e4
Bill <william.hine2019@gmail.com>
2020-07-16 11:52:19 -0400

    Added InsurancesModel

---
+++ Charter/Models/InsurancesModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Charter.Models
+{
+    public class InsurancesModel
+    {
+        [Key]
+        private int InsuranceId { get; set; }
+
+        [Display(Name = "Company Name ")]
+        private string CompanyName { get; set; }
+
+        [Display(Name = "Due Date ")]
+        private DateTime DueDate { get; set; }
+
+        [Display(Name ="Amount Due ")]
+        private double AmountDue { get; set; }
+
+    }
+}
