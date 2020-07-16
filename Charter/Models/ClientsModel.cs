1aadba66a650325008d630a5917fc3ebeba1cc52
Bill <william.hine2019@gmail.com>
2020-07-16 11:38:39 -0400

    Added Client Model

---
+++ Charter/Models/ClientsModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Charter.Models
+{
+    public class ClientsModel
+    {
+        [Key]
+        private int ClientId { get; set; }
+
+        [Display(Name = "First Name ")]
+        private string FirstName { get; set; }
+
+        [Display(Name = "Last Name ")]
+        private string LastName { get; set; }
+
+        [Display(Name = "Email Address ")]
+        private string EmailAddress { get; set; }
+
+        [Display(Name = "Telephone ")]
+        private string Telephone { get; set; }
+
+        [ForeignKey("AddressId")]
+        public int? AddressId { get; set; }
+        public AddressModel AddressModel { get; set; }
+
+        [ForeignKey("CaptainId")]
+        public int? CaptainId { get; set; }
+        public CaptainsModel CaptainsModel { get; set; }
+    }
+}
