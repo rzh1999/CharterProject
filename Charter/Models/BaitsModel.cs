0779a044e1d4196e226d8f11ff72905783359200
Bill <william.hine2019@gmail.com>
2020-07-16 11:56:45 -0400

    Added BaitsModel

---
+++ Charter/Models/BaitsModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Charter.Models
+{
+    public class BaitsModel
+    {
+        [Key]
+        private int BaitId { get; set; }
+
+        [Display(Name = "Bait Type ")]
+        private string BaitType { get; set; }
+
+        [Display(Name = "Cost ")]
+        private double BaitCost { get; set; }
+
+        [Display(Name = "Death Count ")]
+        private int DeathCount { get; set; }
+
+        [ForeignKey("CaptainId")]
+        public int? CaptainId { get; set; }
+        public CaptainsModel CaptainsModel { get; set; }
+
+
+    }
+}
