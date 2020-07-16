d19950760268b66c2d340dcae7ea6cda2e5d231a
Bill <william.hine2019@gmail.com>
2020-07-16 11:14:07 -0400

    Added Address Model

---
+++ Charter/Models/AddressModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Charter.Models
+{
+    public class AddressModel
+    {
+        [Key]
+        private int AddressId { get; set; }
+
+        [Display(Name = "Address ")]
+        private string Address { get; set; }
+
+        [Display(Name = "City ")]
+        private string City { get; set; }
+
+        [Display(Name = "State ")]
+        private string State { get; set; }
+
+        [Display(Name = "Zip Code ")]
+        private string ZipCode { get; set; }
+    }
+}
