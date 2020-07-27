ef348f49006ad5b9fe9856a4014510df346fe6e2
Bill <william.hine2019@gmail.com>
2020-07-27 13:20:46 -0400

    added messages to menu

--- Charter/Controllers/MessagesController.cs
+++ Charter/Controllers/MessagesController.cs
@@ -25,11 +25,7 @@ namespace Charter.Controllers
             return View(messageList);
         }
 
-        // GET: Messages/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
+       
 
         // GET: Messages/Create
         public ActionResult CreateMessage()
@@ -47,52 +43,6 @@ namespace Charter.Controllers
             {
                 _context.messages.Add(messagesModel);
                 _context.SaveChanges();
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Messages/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Messages/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Messages/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Messages/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
 
                 return RedirectToAction(nameof(Index));
             }
